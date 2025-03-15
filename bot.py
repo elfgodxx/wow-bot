326fe0fe35f84cbacf16d911211dd90ff001e1e2
jiasamonte <37324693+jiasamonte@users.noreply.github.com>
2025-03-15 21:20:47 +0800

    add new command for search

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def on_ready():
 
 
 @bot.command()
-async def search(ctx, *, query: str):
+async def wowsearch(ctx, *, query: str):
     search_url = f"https://www.google.com/search?q={query}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
